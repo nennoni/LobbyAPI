09af182cd6e0cb68c0d4b61a234837ba2fcda9ad
nilss <id23jnn@cs.umu.se>
2026-05-26 15:21:19 +0200

    testar något nytt grrrr förra funkade inte

--- LobbyAPI/Program.cs
+++ LobbyAPI/Program.cs
@@ -15,9 +15,10 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.AllowAnyOrigin()
+        policy.SetIsOriginAllowed(_ => true)
               .AllowAnyMethod()
-              .AllowAnyHeader();
+              .AllowAnyHeader()
+              .AllowCredentials();
     });
 });
 
