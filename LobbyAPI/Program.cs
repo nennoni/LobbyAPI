749d9b714b83f940d5da4bbfab335cb0c0d7b885
nilss <id23jnn@cs.umu.se>
2026-05-26 14:55:13 +0200

    Tog bort openAI ur program.cs

--- LobbyAPI/Program.cs
+++ LobbyAPI/Program.cs
@@ -7,17 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
 builder.Services.AddSingleton<LobbyHandler>();
 builder.Services.AddSignalR();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
